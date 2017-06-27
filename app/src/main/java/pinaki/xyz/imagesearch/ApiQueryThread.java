cc1ca524202b62652327799454c386e7f4cf6074
Pinaki Sinha <spinaki@gmail.com>
2017-06-27 05:13:48 -0700

    back and forth between ui thread and worker

--- app/src/main/java/pinaki/xyz/imagesearch/ApiQueryThread.java
+++ app/src/main/java/pinaki/xyz/imagesearch/ApiQueryThread.java
@@ -3,7 +3,6 @@ package pinaki.xyz.imagesearch;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
-import android.util.Log;
 
 import java.util.List;
 
@@ -27,11 +26,18 @@ import pinaki.xyz.imagesearch.data.DataManager;
             public boolean handleMessage(Message msg) {
                 // TODO: query should be parsed from msg
                 // Whethers its thumbnail or full res should be parsed from WHAT
-                List<String> l = DataManager.getInstance().queryThumbNails("Cat");
-                Log.i(TAG, l.get(0));
+                List<String> l = null;
+                switch (msg.what) {
+                    case MainActivity.THUMBNAIL_DOWNLOAD:
+                        String query = (String) msg.obj;
+                        l = DataManager.getInstance().queryThumbNails(query);
+                        break;
+                    default:
+                        break;
+                }
                 // POST THE RESULTS BACK TO mResponseHandler
-                if (mResponseHandler != null) {
-
+                if (mResponseHandler != null && l != null) {
+                    mResponseHandler.obtainMessage(msg.what, l.get(0)).sendToTarget();
                 }
 //                msg.recycle();
                 return true;
@@ -40,6 +46,6 @@ import pinaki.xyz.imagesearch.data.DataManager;
     }
 
     /* package */ void queueQuery(String query) {
-        apiQueryHandler.obtainMessage(1, query).sendToTarget();
+        apiQueryHandler.obtainMessage(MainActivity.THUMBNAIL_DOWNLOAD, query).sendToTarget();
     }
 }
--- app/src/main/java/pinaki/xyz/imagesearch/MainActivity.java
+++ app/src/main/java/pinaki/xyz/imagesearch/MainActivity.java
@@ -5,11 +5,12 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    /* package */ static final int THUMBNAIL_DOWNLOAD = 1;
     private ApiQueryThread  queryThread;
-    Handler workerHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +21,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-//        queryThread = new HandlerThread(TAG);
-//        queryThread.start();
-//        Handler workerHandler = new Handler(queryThread.getLooper());
-//        workerHandler.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                List<String> l = DataManager.getInstance().queryThumbNails("Cat");
-//                Log.i(TAG, l.get(0));
-//            }
-//        });
         queryThread = new ApiQueryThread(createUIHandler());
         queryThread.start();
         queryThread.prepareHandler();
-        queryThread.queueQuery("Cat"); // example query
-
-//        workerHandler.sendMessage();
-//        Handler h = new Handler();
+        queryThread.queueQuery("India"); // example query TODO: get stuff from UI input.
     }
 
     private void startWorkerThread() {
@@ -48,12 +36,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean handleMessage(Message msg) {
                 // GET THE URLs and do stuff here
-//                msg.a
                 switch (msg.what) {
                     // if SEARCH URL DOWNLOAD COMPLETED RENDER THEM
-                    // if HIGH RES DNLD COMPLETED RENDER THEm
+                    case THUMBNAIL_DOWNLOAD:
+                        String url = (String) msg.obj;
+                        Log.i(TAG, url);
+                        break;
+                    default:
+                        break;
+                    // if HIGH RES DNLD COMPLETED RENDER THEM
                 }
-                return false;
+                return true;
             }
         });
         return uiHandler;
